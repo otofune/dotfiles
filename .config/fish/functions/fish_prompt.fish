34498dbc23dd4a1bb9fbfc94ea16e4534765e299
otofune <otofune@gmail.com>
2018-11-21 19:38:11 +0900

    fish_prompt/git-support: prevent conflict marker remaining

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -5,13 +5,20 @@ function fish_prompt
 	echo -n (prompt_pwd)
 	set_color normal
 	if [ -d .git ]
-		set_color brred
+		# branch/commit
+		set_color brblue
 		set gs (git rev-parse --short HEAD)
 		set branch (git symbolic-ref --short -q HEAD)
 		if test $status -eq 0
 			set gs $branch
 		end
 		echo -n " ("$gs")"
+
+		set_color brred
+		set c (git diff (git rev-list --max-parents=0 HEAD) --check | grep 'conflict marker' | wc -l | string trim)
+		if [ $c -ne 0 ]
+			echo -n " !"
+		end
         end
 	set_color normal
         echo -n ' $ '
