2ba85caa2937911c2993bed1b570a30184eca20f
otofune <otofune@gmail.com>
2018-11-21 17:02:28 +0900

    fish_prompt/git-support: support detached HEAD

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -6,7 +6,12 @@ function fish_prompt
 	set_color normal
 	if [ -d .git ]
 		set_color brred
-		echo -n " ("(git symbolic-ref --short HEAD)")"
+		set gs (git rev-parse --short HEAD)
+		set branch (git symbolic-ref --short -q HEAD)
+		if test $status -eq 0
+			set gs $branch
+		end
+		echo -n " ("$gs")"
         end
 	set_color normal
         echo -n ' $ '
