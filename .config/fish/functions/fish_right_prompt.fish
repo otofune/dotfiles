d5e1be3980a6553be1a1af481c3e51b54f4d0c58
otofune <otofune@gmail.com>
2018-12-12 13:46:50 +0900

    Check branch is available when there are no commit

--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -2,22 +2,18 @@
 
 function fish_right_prompt
 	if test (git rev-parse --is-inside-work-tree) = "true"
-		set_color brred
+		set_color brblue
 
 		git rev-parse HEAD > /dev/null 2>&1
-		if [ $status -ne 0 ]
-			echo -n "(Not committed yet)"
-			set_color normal
-			return
+		if [ $status -eq 0 ]
+			set c (git diff (git rev-list --max-parents=0 HEAD) --check | grep 'conflict marker' | wc -l | string trim)
+			if [ $c -ne 0 ]
+				set_color brred
+				echo -n "!"
+			end
+			set gs (git rev-parse --short HEAD)
 		end
 
-		set c (git diff (git rev-list --max-parents=0 HEAD) --check | grep 'conflict marker' | wc -l | string trim)
-		if [ $c -ne 0 ]
-			echo -n "!"
-		end
-
-		set_color brblue
-		set gs (git rev-parse --short HEAD)
 		set branch (git symbolic-ref --short -q HEAD)
 		if test $status -eq 0
 			set gs $branch
