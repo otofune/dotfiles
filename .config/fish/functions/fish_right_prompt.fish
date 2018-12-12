3c5d0aa5f511f2a839d7af5469ba9f933974b358
otofune <otofune@gmail.com>
2018-12-12 13:44:15 +0900

    show 'Not committed yet'

--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -3,6 +3,14 @@
 function fish_right_prompt
 	if test (git rev-parse --is-inside-work-tree) = "true"
 		set_color brred
+
+		git rev-parse HEAD > /dev/null 2>&1
+		if [ $status -ne 0 ]
+			echo -n "(Not committed yet)"
+			set_color normal
+			return
+		end
+
 		set c (git diff (git rev-list --max-parents=0 HEAD) --check | grep 'conflict marker' | wc -l | string trim)
 		if [ $c -ne 0 ]
 			echo -n "!"
