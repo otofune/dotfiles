3ee642cb663217a61281a51a0aacc19a1f2ba80b
otofune <otofune@otofune.me>
2019-08-16 16:02:13 +0900

    fish: make __git_fetch silent

--- platform-all/replacement/.config/fish/on-variable-handlers.fish
+++ platform-all/replacement/.config/fish/on-variable-handlers.fish
@@ -5,7 +5,7 @@ function __git_fetch --on-variable PWD --description 'git fetch on entering dire
     if test $status -ne 128
         if ls .git 1>/dev/null 2>/dev/null
             # ignore moving from sub-directory
-            if not string match -- "$PWD*" $PPWD
+            if not string match -q -- "$PWD*" $PPWD
                 git fetch
             end
         end
