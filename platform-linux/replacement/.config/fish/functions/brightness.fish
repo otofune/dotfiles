622a28c41b95752ee36496d462e1db0b2a768897
otofune <otofune@otofune.me>
2019-08-18 01:29:52 +0900

    :fish: :penguin: brightness: Prevent to use invalid value

--- platform-linux/replacement/.config/fish/functions/brightness.fish
+++ platform-linux/replacement/.config/fish/functions/brightness.fish
@@ -1,9 +1,14 @@
 # (c) otofune
 
 function brightness
+  set max_brightness (cat /sys/class/backlight/*/max_brightness)
   set level $argv[1]
   if test "$level" != ""
-    echo $level | sudo tee /sys/class/backlight/*/brightness
+    if test $level -gt $max_brightness
+      echo "Invalid level specified." 1>&2
+      return 1
+    end
+    echo $level | sudo tee /sys/class/backlight/*/brightness 1>/dev/null
     return
   end
   cat /sys/class/backlight/*/brightness
