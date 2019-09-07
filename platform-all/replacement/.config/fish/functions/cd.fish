2ce4e8a82c920812f93c683292b714094224ac6a
otofune <otofune@otofune.me>
2019-09-07 11:32:59 +0900

    :fish: Do not exec when no directory selected

--- platform-all/replacement/.config/fish/functions/cd.fish
+++ platform-all/replacement/.config/fish/functions/cd.fish
@@ -8,6 +8,9 @@ function cd --wraps=cd
   set length (count $argv)
   if test $length -eq 0
     set repository (ghq list | fzf --exit-0)
+    if test (string length "$repository") -eq 0
+      return 1
+    end
     builtin cd (ghq root)/$repository
     return
   end
--- platform-all/replacement/.config/fish/functions/code.fish
+++ platform-all/replacement/.config/fish/functions/code.fish
@@ -4,6 +4,9 @@ function code --wraps=code
   set length (count $argv)
   if test $length -eq 0
     set repository (ghq list | fzf --exit-0)
+    if test (string length "$repository") -eq 0
+      return 1
+    end
     command code (ghq root)/$repository
     return
   end
