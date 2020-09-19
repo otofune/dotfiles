3b805d030fb74563cccd7002dd300228efaedd92
otofune <otofune@otofune.me>
2020-09-19 14:34:23 +0900

    :apple: Add OpenSSL to LIBRARY_PATH

--- platform-darwin/replacement/.config/fish/environment.fish
+++ platform-darwin/replacement/.config/fish/environment.fish
@@ -3,3 +3,5 @@
 ls ~/Library/LaunchAgents/local* | xargs -I% launchctl load % 2>/dev/null
 
 set -x fish_user_paths /usr/local/opt/libpq/bin $fish_user_paths
+
+export LIBRARY_PATH="$LIBRARY_PATH:/usr/local/opt/openssl/lib/"
